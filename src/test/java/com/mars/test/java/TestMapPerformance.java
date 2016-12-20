027214d177edc6c2a7c544af95df546cf5cc87ea
marsyang <bcdex34567890>
2016-12-20 17:05:32 +0800

    add Map Performance Test

---
+++ src/test/java/com/mars/test/java/TestMapPerformance.java
@@ -0,0 +1,19 @@
+package com.mars.test.java;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by USER on 2016/12/20.
+ */
+@Slf4j
+public class TestMapPerformance {
+
+    public void testKeySet(){
+
+    }
+
+    public void testEntryset(){
+
+    }
+
+}
