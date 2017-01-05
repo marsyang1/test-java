254eda359ec891e0fd049447e1ad662b81dd41ce
marsyang <bcdex34567890>
2017-01-05 10:49:09 +0800

    add hashing

---
+++ src/test/java/com/mars/test/java/TestIo.java
@@ -0,0 +1,34 @@
+package com.mars.test.java;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Random;
+
+/**
+ * Created by USER on 2016/12/1.
+ */
+@Slf4j
+public class TestIo {
+
+
+    @Test
+    public void testInputStream() throws IOException {
+//        BufferedReader reader = new BufferedReader(new InputStreamReader("")));
+//        StringBuffer stringBuffer = new StringBuffer();
+//        String str = "";
+//        while ((str = reader.readLine()) != null) {
+//            stringBuffer.append(str);
+//        }
+//        String ts = stringBuffer.toString();
+//        log.info("ts");
+    }
+
+}
