a9c9c97c91dcf96139fa82ad41992a85a7aeb06f
yangyuchi <marsyang1@gmail.com>
2017-02-23 01:02:18 +0800

    add reference

---
+++ README.md
@@ -0,0 +1,4 @@
+* concurrency
+    * https://dzone.com/articles/synchronized-vs-lock
+    * http://lycog.com/concurency/performance-reentrantlock-synchronized/
+    
--- src/main/java/com/mars/test/design/Counter.java
+++ src/main/java/com/mars/test/design/Counter.java
@@ -2,7 +2,7 @@ package com.mars.test.design;
 
 /**
  * Created by yangyuchi on 23/02/2017.
- * http://lycog.com/concurency/performance-reentrantlock-synchronized/
+ *
  */
 public interface Counter {
 
