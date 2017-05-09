e4b1ffaef98ae602dfc1f068b1cded76571e5b17
marsyang <bcdex34567890>
2017-05-09 10:39:20 +0800

    add ok http post

--- src/test/java/com/mars/test/gson/TestConvertDate.java
+++ src/test/java/com/mars/test/gson/TestConvertDate.java
@@ -2,7 +2,11 @@ package com.mars.test.gson;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 
 import java.util.Date;
@@ -13,17 +17,38 @@ import java.util.Date;
 @Slf4j
 public class TestConvertDate {
 
+    final Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd HH:mm:ss").create();
+
     @Test
-    public void test1(){
+    public void test1() {
         String json = new Gson().toJson(new Date());
         log.info("Date to json by default json =" + json);
     }
 
     @Test
-    public void testSetDateTimeFormat(){
-        String json = new GsonBuilder()
-                .setDateFormat("yyyy-MM-dd HH:mm:ss").create().toJson(new Date());
+    public void testSetDateTimeFormat() {
+        String json = gson.toJson(new Date());
         log.info("Date to json with format =" + json);
+    }
+
+    @Test
+    public void testDateTimeFormat() {
+        TestData data = new TestData("1234"
+                , DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").parseDateTime("2017-01-24 00:00:00").toDate());
+        String json = gson.toJson(data);
+        log.info("Date to json with format =" + json);
+        data = gson.fromJson(json,TestData.class);
+        log.info("data = " + data.toString());
+    }
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    class TestData {
+        private String name;
+        private Date date;
     }
 
 
