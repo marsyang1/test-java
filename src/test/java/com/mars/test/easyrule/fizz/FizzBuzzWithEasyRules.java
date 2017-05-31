d403d1beb216f4b9baec1ec0986f9ad06e197174
marsyang <bcdex34567890>
2017-05-31 13:14:56 +0800

    change fizz test

--- src/test/java/com/mars/test/easyrule/fizz/FizzBuzzWithEasyRules.java
+++ src/test/java/com/mars/test/easyrule/fizz/FizzBuzzWithEasyRules.java
@@ -2,10 +2,14 @@ package com.mars.test.easyrule.fizz;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rule;
 import org.jeasy.rules.api.Rules;
 import org.jeasy.rules.api.RulesEngine;
 import org.jeasy.rules.core.RulesEngineBuilder;
 import org.junit.Test;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by USER on 2017/5/31.
@@ -32,6 +36,33 @@ public class FizzBuzzWithEasyRules {
         for (int i = 1; i <= 100; i++) {
             facts.put("number", i);
             fizzBuzzEngine.fire(rules, facts);
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void test2(){
+        RulesEngine fizzBuzzEngine = RulesEngineBuilder.aNewRulesEngine()
+                .withSkipOnFirstAppliedRule(true)
+                .withSilentMode(true)
+                .build();
+
+        // create rules
+        Rules rules = new Rules();
+        rules.register(new FizzRule());
+        rules.register(new BuzzRule());
+        rules.register(new FizzBuzzRule(new FizzRule(), new BuzzRule()));
+        rules.register(new NonFizzBuzzRule());
+
+        // fire rules
+        Facts facts = new Facts();
+        for (int i = 1; i <= 21; i++) {
+            facts.put("number", i);
+            Map<Rule, Boolean> check = fizzBuzzEngine.check(rules, facts);
+            check.forEach((k,v)-> {
+                log.info("rule = " + k.toString());
+                log.info("value = "+ v.toString());
+            });
             System.out.println();
         }
     }
